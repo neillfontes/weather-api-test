268f4189328cd1d1d2a32a9be8e6fa99e28805fb
Neill Lima <neillfontes@gmail.com>
2016-12-05 11:26:23 +0100

    Add tests per country, city, cityid, geolocation

--- src/test/java/com/fyber/weather/test/APITest.java
+++ src/test/java/com/fyber/weather/test/APITest.java
@@ -87,22 +87,6 @@ public class APITest {
 	}
 	
 	@Test
-	public void shouldAuthenticateAndReturnWeatherForGermanyWhenAPIKeyIsValidAndRequestParamsAreValid() {
-		
-		String country = "Germany";
-		
-		given().
-			baseUri(clientConfig.getBaseURL()).
-			contentType(clientConfig.getContentType()).
-			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
-		when().
-			get("/weather?q={country}", country).
-		then().
-			statusCode(200);
-		
-	}
-	
-	@Test
 	public void shouldAuthenticateAndReturnWeatherForGermanyWhenAPIKeyIsValidAndRequestParamsAreValidAndContentTypeIsNotApplicationJSON() {
 		
 		String country = "Germany";
@@ -114,10 +98,101 @@ public class APITest {
 		when().
 			get("/weather?q={country}", country).
 		then().
-			statusCode(200);
+			statusCode(200).
+			body("cod", equalTo(200));
 		
 	}
 	
+	@Test
+	public void shouldAuthenticateAndReturnWeatherForGermanyWhenAPIKeyIsValidAndRequestParamsAreValid() {
+		
+		String country = "Germany";
+		
+		given().
+			baseUri(clientConfig.getBaseURL()).
+			contentType(clientConfig.getContentType()).
+			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
+		when().
+			get("/weather?q={country}", country).
+		then().
+			statusCode(200).
+			body("cod", equalTo(200));
+		
+	}
 	
+	@Test
+	public void shouldAuthenticateAndReturnWeatherForBerlinAndGermanyWhenAPIKeyIsValidAndRequestParamsAreValid() {
+		
+		String city = "Berlin";
+		String country = "DE";
+		
+		given().
+			baseUri(clientConfig.getBaseURL()).
+			contentType(clientConfig.getContentType()).
+			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
+		when().
+			get("/weather?q={city},{country}", city, country).
+		then().
+			statusCode(200).
+			body("cod", equalTo(200));
+		
+	}
+	
+	@Test
+	public void shouldAuthenticateAndReturnWeatherForBerlinWhenAPIKeyIsValidAndRequestParamsAreValid() {
+		
+		String city = "Berlin";
+		
+		given().
+			baseUri(clientConfig.getBaseURL()).
+			contentType(clientConfig.getContentType()).
+			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
+		when().
+			get("/weather?q={city}", city).
+		then().
+			statusCode(200).
+			body("cod", equalTo(200));
+		
+	}
+	
+	@Test
+	public void shouldAuthenticateAndReturnWeatherForBerlinAsCityIDWhenAPIKeyIsValidAndRequestParamsAreValid() {
+		
+		String cityID = "2950159";
+		
+		given().
+			baseUri(clientConfig.getBaseURL()).
+			contentType(clientConfig.getContentType()).
+			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
+		when().
+			get("/weather?id={cityID}", cityID).
+		then().
+			statusCode(200).
+			body("cod", 	equalTo(200), 
+				 "name", 	equalTo("Berlin"));
+		
+	}
+		
+	@Test
+	public void shouldAuthenticateAndReturnWeatherForBerlinAsGeoCoordinatesWhenAPIKeyIsValidAndRequestParamsAreValid() {
+		
+		String lat = "52.524368";
+		String lon = "13.41053";
+		
+//		float lat = 35;
+//		float lon = 139;
+		
+		given().
+			baseUri(clientConfig.getBaseURL()).
+			contentType(clientConfig.getContentType()).
+			header(clientConfig.getAuthHeader(), clientConfig.getApiKey()).
+		when().
+			get("/weather?lat={lat}&lon={lon}", lat, lon).
+		then().
+			statusCode(200).
+			body("cod", 	equalTo(200), 
+				 "name", 	equalTo("Berlin Mitte"));
+		
+	}
 	
 }
